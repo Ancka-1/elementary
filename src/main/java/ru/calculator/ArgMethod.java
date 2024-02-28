682dc7d763794655ce210c8241137951a457cedf
Ancka-1 <Annausa91@gmail.com>
2024-02-28 22:58:27 +0200

    1.6 Arguments

---
+++ src/main/java/ru/calculator/ArgMethod.java
@@ -0,0 +1,20 @@
+package ru.calculator;
+
+public class ArgMethod {
+    public static void hello(String name, int age) {
+        System.out.println("Hello," + name + ",age = " + age);
+    }
+
+    public static void main(String[] args) {
+        String name = "Java";
+        int age = 6;
+       // ArgMethod.hello("Java");
+        ArgMethod.hello("Java", 6);
+        ArgMethod.hello("Java", 6);
+        ArgMethod.hello("Java", 6);
+        ArgMethod.hello("Java", 6);
+
+       // ArgMethod.hello("Java");
+       // ArgMethod.hello(6, "Java");
+    }
+}
