af9a4105cb12fadc9b40be4c4c01841183b876b5
Ancka-1 <Annausa91@gmail.com>
2024-03-13 23:05:31 +0200

    1.10 ConverterTest

---
+++ src/test/java/ru/converter/ConverterTest.java
@@ -0,0 +1,43 @@
+package ru.converter;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConverterTest {
+    @Test
+    void whenConvert140RblThen1Dot4Euro() {
+        float in = 140;
+        float expected = 1.4f;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void whenConvert200RblThen2dot2222Dlr() {
+        float in = 200;
+        float expected = 2.2222f;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void whenConvert300RblThen33Dot3333Dlr() {
+        float in = 300;
+        float expected = 3.3333f;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void whenConvert100RblThen1Euro() {
+        float in = 100;
+        float expected = 1f;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+}
