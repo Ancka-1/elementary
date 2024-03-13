4d43582a6fb2293137625ac00414f4d0957bbadc
Ancka-1 <Annausa91@gmail.com>
2024-03-13 22:14:56 +0200

    1.9 Lesson4 + String

--- src/main/java/ru/calculate/Out.java
+++ src/main/java/ru/calculate/Out.java
@@ -13,7 +13,16 @@ public class Out {
         String id2 = name + separate + age + " years old";
         System.out.println(id);
         System.out.println(id2);
-
+        char a = 'A';
+        char b = 'B';
+        System.out.println(a + b);
+        char c1 = '\u0035';
+        char c2 = '\u004D';
+        char c3 = '\u006E';
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println("\u0031\u0032\u0033");
     }
 
 }
--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -19,7 +19,5 @@ public class Calculator {
         plus(100, 500);
         minus(4, 2);
         minus(3, 5);
-
     }
-
 }
