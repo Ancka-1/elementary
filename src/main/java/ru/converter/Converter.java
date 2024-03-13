9c7792a6065f7fdf6bd1ef6425e265d5e6af1a62
Ancka-1 <Annausa91@gmail.com>
2024-03-13 22:37:36 +0200

    1.10 Testing

--- src/main/java/ru/converter/Converter.java
+++ src/main/java/ru/converter/Converter.java
@@ -9,10 +9,16 @@ public class Converter {
         return value / 90;
     }
 
-    public static void main(String[] arg) {
-        float euro = Converter.rubleToEuro(100);
-        float dollar = Converter.rubleToDollar(90);
-        System.out.println("100 rubles are " + euro + "  euro");
-        System.out.println("90 rubles are " + dollar + "  dollar");
+    public static void main(String[] args) {
+        float in = 140;
+        float expected = 1.4f;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 1.4. Test result : " + passed);
+        in = 180;
+        expected = 2;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("180 rubles are 2. Test result : " + passed);
     }
 }
