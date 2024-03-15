c24454c8ceb49b473d498dd5fa49db3a48fc3090
Ancka-1 <Annausa91@gmail.com>
2024-03-15 11:04:48 +0200

    2.4 Ideal Weight Test

--- src/main/java/ru/calculator/Fit.java
+++ src/main/java/ru/calculator/Fit.java
@@ -2,15 +2,17 @@ package ru.calculator;
 
 public class Fit {
     public static double manWeight(short height) {
-        return (height - 100) * 1.15;
+       return (height - 100) * 1.15;
+
     }
 
     public static double womanWeight(short height) {
-        return (height - 110) * 1.15;
+     return (height - 110) * 1.15;
+
     }
 
     public static void main(String[] args) {
-        short height = 187;
+        short height = 162;
         double man = Fit.manWeight(height);
         double woman = Fit.womanWeight(height);
         System.out.println("Man 187 is " +  man);
