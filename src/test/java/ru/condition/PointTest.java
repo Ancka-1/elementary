f382adafe9928ab60518c92e90c3f702bc860ecb
Ancka-1 <Annausa91@gmail.com>
2024-03-17 21:58:56 +0200

    2.5 Test distance between dots

---
+++ src/test/java/ru/condition/PointTest.java
@@ -0,0 +1,43 @@
+package ru.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class PointTest {
+
+    @Test
+    void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when01to10then1Dot41() {
+        double expected = 1.41;
+        int x1 = 0;
+        int y1 = 1;
+        int x2 = 1;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when11to21then1() {
+        double expected = 1;
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 2;
+        int y2 = 1;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+}
