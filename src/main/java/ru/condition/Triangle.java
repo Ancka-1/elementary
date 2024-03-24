69744e98dbd9cf6180583e8aa9d7fc26366bc665
Ancka-1 <Annausa91@gmail.com>
2024-03-24 22:58:58 +0200

    3.10 ifEse short

---
+++ src/main/java/ru/condition/Triangle.java
@@ -0,0 +1,7 @@
+package ru.condition;
+
+public class Triangle {
+    public static boolean exist(double ab, double ac, double bc) {
+        return ab + bc > ac && ab + ac > bc && bc + ac > ab;
+    }
+}
